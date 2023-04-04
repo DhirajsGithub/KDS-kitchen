6c22238e93d5b081550abb83085fd4e5ae6299ea
Dhiraj Borse <borsedhiraj123@gmail.com>
2023-04-04 15:23:34 +0530

    small changes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 
 # KDS-kitchen kitchen dept page 
+
+## to start it on lcoal device use the following commands : 
+git clone https://github.com/DhirajsGithub/KDS-kitchen.git
+
+cd KDS-kitchen
+
+npm install
+
+npm start
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { OrderContextProvider } from "./store/orders-store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <OrderContextProvider>
         <App />
       </OrderContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
